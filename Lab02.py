999511299b67303f407a66071de13ff4b716f800
m1chaelv <47853397+m1chaelv@users.noreply.github.com>
2023-09-18 15:13:38 -0500

    Update Lab02.py

--- Lab02.py
+++ Lab02.py
@@ -4,58 +4,63 @@
 #
 #  Program #:         Lab 2
 #
-#  File Name:         Lab1.py
+#  File Name:         Lab02.py
 #
 #  Course:            COSC 1336 Programming Fundamentals I
 #
-#  Due Date:          09-18-2023
+#  Due Date:          09-20-2023
 #
 #  Instructor:        Onabajo
 #
-#  Chapter:           <Chapter 2>
+#  Chapter:           <Chapter 3>
 #
 #  Description:
-#   A movie theater only keeps a percentage of the revenue earned from ticket
-#   sales. The remainder goes to the movie company. Write a program that
-#   calculates a theater's gross and net box office profit for a night. The program
-#   should ask for the name of the movie, and how many adult and children
-#   tickets were sold. (The price of an adult ticket is $6.00 and a child's ticket is
-#   $3.00). It should display a report similar to the one below:
+#	In shopping for a new house, you must consider several factors. 
+#	In this problem, the initial cost of the house, the estimated 
+#	annual fuel cost, and the annual tax rate are available.. Write 
+#	a program that will determine the total cost of a house after a 
+#	five-year period for the following set of data.. You should be 
+#	able to inspect your program output to determine the best buy.
 #
-#   Note: Assume that the theatre keeps 20% of the gross box profit.
+#	Initial cost	Annual Fuel Cost	Tax Rate
+#	$167,000	$2,500			0.025
+#	$175,000	$2,300			0.025
+#	$162,000	$1,850			0.020
+#
+#	To calculate the house cost, add the initial cost to the fuel 
+#	cost for five years, then add the taxes for five years. Taxes 
+#	for one year are computed by multiplying the tax rate by the 
+#	initial cost. Write a program segment to display instructions 
+#	to the program user.
 #
 #***************************************************************
 
 #***************************************************************
 #
 #  Function:     main
-#
 #  Description:  The main function of the program
-#
 #  Parameters:   None
-#
 #  Returns:      Nothing
 #
 #**************************************************************
+
 def main():
-
-    developerInfo() # Signature
-
-
-
+    # Student Header
+    developerInfo()
 
     # Initialize variables
     H1 = 'House 1'
     H2 = 'House 2'
     H3 = 'House 3'
 
+    # Display "Welcome" House Graphic
+    house()
+
     # Request Inputs
     # To expedite testing pre-populated data will bypass user prompts.
     # set USE_TEST to True to use test data. Set to False before promoting to prod.
-    USE_TEST = True
-
-    house()
-
+    USE_TEST = False
+    
     if USE_TEST:
         House1_init_cost = 167000
         House1_annual_fuel = 2300
@@ -73,7 +78,7 @@ def main():
         print('Enter the following information for House 1.')
         House1_init_cost = float(input('Initial cost: '))
         House1_annual_fuel = float(input('Annual fuel cost: '))
-        House1_tax_rate = float(input('Tax rate: '))
+        House1_tax_rate = float(input('Tax rate (ex:2.5 or .025 = 2.5%): '))
         if House1_tax_rate >= 1:
             House1_tax_rate = House1_tax_rate/100
         next()
@@ -81,7 +86,10 @@ def main():
         print('Enter the following information for House 2.')
         House2_init_cost = float(input('Initial cost: '))
         House2_annual_fuel = float(input('Annual fuel cost: '))
-        House2_tax_rate = float(input('Tax rate: '))
+        House2_tax_rate = float(input('Tax rate (ex:2.5 or .025 = 2.5%): '))
+        # Data cleansing and validation
+        # If tax rate entered is greater than 1, assumption is user is entering whole number
+        # Input data is adjusted by dividing by 100
         if House2_tax_rate >= 1:
             House2_tax_rate = House2_tax_rate/100
         next()
@@ -89,7 +97,10 @@ def main():
         print('Enter the following information for House 3.')
         House3_init_cost = float(input('Initial cost: '))
         House3_annual_fuel = float(input('Annual fuel cost: '))
-        House3_tax_rate = float(input('Tax rate: '))
+        House3_tax_rate = float(input('Tax rate (ex:2.5 or .025 = 2.5%): '))
+        # Data cleansing and validation
+        # If tax rate entered is greater than 1, assumption is user is entering whole number
+        # Input data is adjusted by dividing by 100
         if House3_tax_rate >= 1:
             House3_tax_rate = House3_tax_rate/100
         next()
@@ -99,12 +110,19 @@ def main():
     next()
 
     # Output
+    # Calculations inline:
+    # 5-Year Costs = Initial Cost + 5 * (Tax Rate * Inital Cost + Annual Fuel Costs)
     print('Summary:')
     print('House\tInitial Cost\t5-Year Costs')
     print(f'{H1}\t${House1_init_cost:,.2f}\t${House1_init_cost+(House1_tax_rate*House1_init_cost+House1_annual_fuel)*5:,.2f}')
     print(f'{H2}\t${House2_init_cost:,.2f}\t${House2_init_cost+(House2_tax_rate*House2_init_cost+House2_annual_fuel)*5:,.2f}')
     print(f'{H3}\t${House3_init_cost:,.2f}\t${House3_init_cost+(House3_tax_rate*House3_init_cost+House3_annual_fuel)*5:,.2f}')
     next()
+
+    # Calculations inline:
+    # 5-Year Fuel Costs = 5 * Annual Fuel Costs
+    # 5-Year Tax Costs = 5 * (Tax Rate * Inital Cost)
+    # 5-Year Overall Costs = Initial Cost + 5-Year Fuel + 5-Year Tax    
     print('Details:')
     print(f'{"":^20}{H1:^20}{H2:^20}{H3:^20}')
     print(f'{"Init Cost":<20}{House1_init_cost:^20,.2f}{House2_init_cost:^20,.2f}{House3_init_cost:^20,.2f}')
@@ -112,25 +130,26 @@ def main():
     print(f'{"5Y Tax Cost":<20}{(House1_tax_rate*House1_init_cost)*5:^20,.2f}{(House2_tax_rate*House2_init_cost)*5:^20,.2f}{(House3_tax_rate*House3_init_cost)*5:^20,.2f}')
     print(f'{"Overall Cost":<20}{House1_init_cost+(House1_tax_rate*House1_init_cost+House1_annual_fuel)*5:^20,.2f}{House2_init_cost+(House2_tax_rate*House2_init_cost+House2_annual_fuel)*5:^20,.2f}{House3_init_cost+(House3_tax_rate*House3_init_cost+House3_annual_fuel)*5:^20,.2f}')
     next()
+
+    # Data Integrity Validation: To give user confirmation of the data accepted and used
+    # 1-Year Fuel Costs
+    # 1-Year Tax Rate
     print('Notes:')
     print(f'{"Annual Fuel Cost":<20}{House1_annual_fuel:^20,.2f}{House2_annual_fuel:^20,.2f}{House3_annual_fuel:^20,.2f}')
-    print(f'{"Tax Rate":<20}{House1_tax_rate:^20%}{House2_tax_rate:^20%}'f'{House3_tax_rate:^20%}')
+    print(f'{"Tax Rate":<20}{House1_tax_rate:^20.3%}{House2_tax_rate:^20.3%}'f'{House3_tax_rate:^20.3%}')
+    next()
 
     # Hold Screen
-    next()
-    dummy=input('Please enter any key to continue...')
-
+    hold()
+    
     # End of the main function
 
 #***************************************************************
 #
 #  Function:     developerInfo
-#
 #  Description:  Prints Programmer's information
-#
 #  Parameters:   None
-#
-#  Returns:      Nothing
+#  Returns:      Output header
 #
 #**************************************************************
 def developerInfo():
@@ -140,11 +159,28 @@ def developerInfo():
     print()
     # End of the developerInfo function
 
+#***************************************************************
+#
+#  Function:     next
+#  Description:  Create a multi-line space to between input/output
+#                to keep screen uncluttered
+#  Parameters:   None
+#  Returns:      3 blank lines
+#
+#**************************************************************
 def next():
     print ()
     print ()
     print ()
 
+#***************************************************************
+#
+#  Function:     house
+#  Description:  Imbelishment to add related visual to program
+#  Parameters:   None
+#  Returns:      Home graphic
+#
+#**************************************************************
 def house():
     next()
     
@@ -164,8 +200,14 @@ def house():
 
     print(house_art)
 
-# Cat graphic print with message
-
+#***************************************************************
+#
+#  Function:     cat
+#  Description:  Imbelishment to add visual and messaging to program
+#  Parameters:   4 (3 customizations for graphic and 1 message)
+#  Returns:      Graphic and Messege
+#
+#**************************************************************
 def cat(ears,face,feet,message):
     # Cat Ears
     earsx = ("  /\_/\   ")
@@ -195,24 +237,17 @@ def cat(ears,face,feet,message):
 
     print(earsx+message+"\n"+facex+"\n"+feetx)
 
-    ImgE1 = ("  (\_/)   ")
-    ImgE2 = ("  (\_(\   ")
-    ImgE3 = ("  /)_/)   ")
-    ImgE4 = ("  /\_/\   ")
-
-    # Cat Face
-    ImgF1 = (" (='x'=)  ")
-    ImgF2 = (" (=-.-=)  ")
-    ImgF3 = (" (=oxo=)  ")
-    ImgF4 = (" (='.'=)  ")
-    ImgF5 = (" (=^.^=)  ")
-
-    # Cat Feet
-    ImgFT1 = ('(") (")_/')
-
-    # Hold Screen
-    next()
+#***************************************************************
+#
+#  Function:     hold
+#  Description:  Prompt to pause the screen until ready
+#  Parameters:   None
+#  Returns:      Nothing
+#
+#**************************************************************
+def hold():
     dummy=input('Please enter any key to continue...')    
+
 
 # Call the main function
 main()
