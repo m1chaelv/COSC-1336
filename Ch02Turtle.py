a6333d84e4f804c89d2ffa089a2c6ef831b7b968
m1chaelv <47853397+m1chaelv@users.noreply.github.com>
2023-09-20 02:39:55 -0500

    Ch3 activity

--- Ch02Turtle.py
+++ Ch02Turtle.py
@@ -1,12 +1,40 @@
 def main():
     import turtle
-    turtle.goto(0,100)
-    turtle.goto(100,100)
-    turtle.goto(100,0)
-    turtle.goto(0,0)
-    turtle.clearscreen()
-    waiting=('ready?')
+#    turtle.goto(0,100)
+#    turtle.goto(100,100)
+#    turtle.goto(100,0)
+#    turtle.goto(0,0)
+#    turtle.clearscreen()
+#    waiting=('ready?')
+#    import turtle
+    turtle.clearscreen
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.goto(0,-40)
+    turtle.begin_fill()
+    turtle.color('red')
+    turtle.penup()
+    turtle.circle(40)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.goto(50,50)
+    turtle.pendown()
+    turtle.color('black')
+    turtle.goto(-50,50)
+    turtle.goto(-50,-50)
+    turtle.goto(50,-50)
+    turtle.goto(50,50)
 
+    number = 2
+
+    if number == 1:
+        print('One')
+    elif number == 2:
+        print('Two')
+    elif number == 3:
+        print('Three')
+    else:
+        print('Unknown')
     
 
 
