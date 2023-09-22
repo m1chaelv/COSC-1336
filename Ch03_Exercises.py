6337084cd1c91225013dbfc3700ddc0995d11c1b
m1chaelv <47853397+m1chaelv@users.noreply.github.com>
2023-09-22 02:08:27 -0500

    exercise 14 start

--- Ch03_Exercises.py
+++ Ch03_Exercises.py
@@ -842,6 +842,22 @@ def exercise13():
     next()
     hold()
 
+
+# Body Mass Index
+# Write a program that calculates and displays a person’s body 
+# mass index (BMI). The BMI is often used to determine whether a 
+# person is overweight or underweight for his or her height. A 
+# person’s BMI is calculated with the following formula:
+# 
+# where weight is measured in pounds and height is measured in inches. 
+# The program should ask the user to enter his or her weight and height, 
+# then display the user’s BMI. The program should also display a message 
+# indicating whether the person has optimal weight, is underweight, or 
+# is overweight. A person’s weight is considered to be optimal if his 
+# or her BMI is between 18.5 and 25. If the BMI is less than 18.5, the 
+# person is considered to be underweight. If the BMI value is greater 
+# than 25, the person is considered to be overweight.
+
 def exercise14():
     next()
 
@@ -881,9 +897,9 @@ def main():
 #    next()
 #    exercise12()
 #    next()
-    exercise13()
+#    exercise13()
 #    next()
-#    exercise14()
+    exercise14()
 #    next()
 #    exercise15()
 
