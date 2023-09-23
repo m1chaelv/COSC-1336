084ed62de5c25575c30814404ccbbcaa1c8dbbad
m1chaelv <47853397+m1chaelv@users.noreply.github.com>
2023-09-22 19:08:58 -0500

    exercise 16

--- Ch03_Exercises.py
+++ Ch03_Exercises.py
@@ -973,11 +973,11 @@ def exercise15():
     M='[Minutes]'
     S='[Seconds]'
     U='-------'
-
-    # initialize variables     
     SECONDS_IN_MINUTE=60
     SECONDS_IN_HOUR=3600
     SECONDS_IN_DAY=86400
+
+    # initialize variables     
     seconds=0
     seconds_remain=0
     minutes=0
@@ -1012,10 +1012,84 @@ def exercise15():
 
 
 
-# PDL
-def exercise16():
-    next()
+# February Days
+# The month of February normally has 28 days. But if it is a leap year, 
+# February has 29 days. Write a program that asks the user to enter a 
+# year. The program should then display the number of days in February 
+# that year. Use the following criteria to identify leap years:
+# 
+# Determine whether the year is divisible by 100. If it is, then it 
+# is a leap year if and only if it is also divisible by 400. For 
+# example, 2000 is a leap year, but 2100 is not.
+# 
+# If the year is not divisible by 100, then it is a leap year if and 
+# only if it is divisible by 4. For example, 2008 is a leap year, 
+# but 2009 is not.
+# 
+# Here is a sample run of the program:
+# 
+# Enter a year: 2008  [Enter]
+#  In 2008 February has 29 days.
 
+# start
+#     init
+#     year=0
+#     days=0
+# 
+#     input
+#     year
+# 
+#     calc
+#     if year/100
+#         then
+#             if year/400
+#                 then
+#                     days=29
+#             else
+#                 days=28
+#     else
+#         if year/4
+#             then
+#                 days=29
+#         else
+#             days=28
+# 
+#     output
+#     year, days
+# stop
+
+def exercise16():
+    # initialize CONSTANT
+    TITLE='February Days'
+    BORDER='---------------------'
+
+    # initialize variables     
+    year=0
+    days=0
+
+    # input
+    print(f'{BORDER:^30}\n{TITLE:^30}\n{BORDER:^30}')
+    next()
+    print('Instructions:')
+    print('Enter a year and the number of days in February')
+    print('will be returned to identify the Leap Years.')
+    next()
+    year=int(input("Enter a year: "))
+    
+    # calculations
+    days=28
+    if year%100==0:
+        if year%400==0:
+            days=29
+    else:
+        if year%4==0:
+            days=29
+
+    # output
+    next()
+    print(f'In {year} February has {days} days.')
+    next()
+    hold()
 
 # PDL
 def exercise17():
@@ -1067,7 +1141,10 @@ def main():
 #    next()
 #    exercise14()
 #    next()
-    exercise15()
+#    exercise15()
+#    next()
+    exercise16()
+
 
 #***************************************************************
 #
