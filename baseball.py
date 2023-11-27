a06dc5ea0709782bd487ec994cb651e26c0abec9
m1chaelv <47853397+m1chaelv@users.noreply.github.com>
2023-11-27 12:21:11 -0600

    In Class 2023-11-27 a

---
+++ baseball.py
@@ -0,0 +1,52 @@
+class baseball():
+    #***************************************************************
+    #  Function:     single
+    #  Description:  calculate single
+    #  Parameters:   none
+    #  Returns:      return same number
+    #**************************************************************
+    def single(self,x):
+            # local variable
+            x1=0
+            # calculate
+            x1=x*1
+            return(x1)
+
+    #***************************************************************
+    #  Function:     double
+    #  Description:  calculate double
+    #  Parameters:   none
+    #  Returns:      multiple of 2
+    #**************************************************************
+    def double(self,x):
+            # local variable
+            x1=0
+            # calculate
+            x1=x*2
+            return(x1)
+
+    #***************************************************************
+    #  Function:     triple
+    #  Description:  calculate triple
+    #  Parameters:   none
+    #  Returns:      multiple of 3
+    #**************************************************************
+    def triple(self,x):
+            # local variable
+            x1=0
+            # calculate
+            x1=x*3
+            return(x1)
+
+    #***************************************************************
+    #  Function:     homerun
+    #  Description:  calculate homerun
+    #  Parameters:   none
+    #  Returns:      multiple of 4
+    #**************************************************************
+    def homerun(self,x):
+            # local variable
+            x1=0
+            # calculate
+            x1=x*4 
+            return(x1)
